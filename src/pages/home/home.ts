4eed23faeeb9586680cdeb71011d28b514ccbbab
Christian Pecson <chpecson@gmail.com>
2018-05-26 13:56:09 +0800

    ADD debounce time on value change of addres IonInput

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,12 +4,13 @@ import { NavController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NetworkTypes, AccountInfoWithMetaData } from 'nem-library';
 
+import 'rxjs/add/operator/debounceTime';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   accountInfo: AccountInfoWithMetaData;
   form: FormGroup;
   network: any;
@@ -25,8 +26,12 @@ export class HomePage {
       address: ['TCVXPBY223WONUWOIVQXTKUDTF4CBG4TCW4FFCUE', Validators.required]
     });
 
-    this.form.controls['network'].valueChanges
-        .subscribe(network => this.nemProvider.changeNetwork(network));
+    this.form.controls['network'].valueChanges.subscribe(network =>
+      this.nemProvider.changeNetwork(network)
+    );
+    this.form.controls['address'].valueChanges.debounceTime(300).subscribe(network =>
+      this.onSubmit(this.form.value)
+    );
   }
 
   onSubmit(form) {
@@ -35,18 +40,21 @@ export class HomePage {
     // this.nemProvider.changeNetwork(form.network);
 
     // Ignore invalid address .
-    if(!isValidAddress) {
+    if (!isValidAddress) {
       alert('Not a valid address');
       return;
     }
 
-    this.nemProvider.getAccountInfoFromAddress(address).subscribe(accountInfo => {
-      this.accountInfo = accountInfo;
-      console.log(accountInfo);
-    }, err => {
-      alert('An error occured. \nView Dev console for more details.');
-      console.error(err);
-    });
+    this.nemProvider.getAccountInfoFromAddress(address).subscribe(
+      accountInfo => {
+        this.accountInfo = accountInfo;
+        console.log(accountInfo);
+      },
+      err => {
+        alert('An error occured. \nView Dev console for more details.');
+        console.error(err);
+      }
+    );
   }
 
   showCosignerDetail(cosigner) {
